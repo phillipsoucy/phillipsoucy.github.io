3549c367472c0ed79422d6485d7b6588721c6546
phillipsoucy <phillip.soucy@gmail.com>
2017-03-21 00:37:12 -0400

    Headers

--- js/init.js
+++ js/init.js
@@ -154,7 +154,7 @@
 	      type: "POST",
 	      url: "inc/sendEmail.php",
 	      data: data,
-		  headers: {'User-Agent':'phillipsoucy'}
+		  headers: {'User-Agent':'phillipsoucy'},
 	      success: function(msg) {
 
             // Message was sent
